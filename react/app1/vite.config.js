b8a8503996237af92d007f8cc844c5e532b6bd61
István Lantos <lantosistvan89@gmail.com>
2025-04-08 15:04:22 +0200

    React komponensek implementálása

--- react/app1/vite.config.js
+++ react/app1/vite.config.js
@@ -1,15 +1,3 @@
-/* import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: {
-    outDir: '../dist/app1',
-    emptyOutDir: true,
-  },
-}); */
-
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
